903670169bc83f8cc6cf6f5676307b27b7544115
Honkyu <yama.sharing3@gmail.com>
2026-08-07 21:55:06 +0900

    構造変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: "docs"
+        outDir: "docs",
+        rolldownOptions: {
+            input: [
+                "site.css",
+                "public/routes/tobu/urban/stations.json",
+                "routes/tobu/urban/index.html",
+                "routes/tobu/urban/index.tsx",
+                "routes/tobu/urban/Page.tsx",
+                "routes/station-list.css",
+                "routes/StationData.ts",
+                "routes/StationsView.tsx"
+            ]
+        }
     }
 })
